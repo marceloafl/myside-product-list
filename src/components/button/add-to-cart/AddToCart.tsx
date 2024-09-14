097246c21c70d992eb2de8f88f321b125a4c3a96
Marcelo Lemes <marceloaflemes@gmail.com>
2024-09-14 19:04:30 -0300

    feat: improving the use of cart context

--- src/components/button/add-to-cart/AddToCart.tsx
+++ src/components/button/add-to-cart/AddToCart.tsx
@@ -1,17 +1,16 @@
 import styles from "@/components/button/add-to-cart/AddToCart.module.css";
-import { CartContext } from "@/contexts/CartContext";
+import { useCartContext } from "@/contexts/CartContext";
 import { ProductType } from "@/types/product";
-import { useContext } from "react";
 
 interface AddToCartButtonProps {
   product: ProductType;
 }
 
 export default function AddToCartButton({ product }: AddToCartButtonProps) {
-  const context = useContext(CartContext);
+  const { addToCart } = useCartContext();
 
   const handleAddToCart = () => {
-    context?.addToCart(product);
+    addToCart(product);
   };
 
   return (
