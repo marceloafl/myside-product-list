c4f11b45c9b22a4899f8da5f78dae6db10967a2a
Marcelo Lemes <marceloaflemes@gmail.com>
2024-09-15 15:42:13 -0300

    fix: import error before build

--- src/components/product-details/ProductDetails.tsx
+++ src/components/product-details/ProductDetails.tsx
@@ -1,5 +1,5 @@
 import styles from "@/components/product-details/ProductDetails.module.css";
-import { mockProducts, ProductType } from "@/types/product";
+import { ProductType } from "@/types/product";
 import { useState } from "react";
 import ProductMainInfo from "@/components/product-main-info/ProductMainInfo";
 import ProductTechInfo from "@/components/product-tech-info/ProductTechInfo";
